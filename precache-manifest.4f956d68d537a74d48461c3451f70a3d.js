788401621884c245422b1e34a25d9f1f3c072f75
Sea Light <sigal@sigals-MacBook-Pro.local>
2022-01-25 17:16:17 +0200

    Updates

---
+++ precache-manifest.4f956d68d537a74d48461c3451f70a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8ded4c13c834bbd6c66f3c1a97ea16",
+    "url": "/weather_app_1/index.html"
+  },
+  {
+    "revision": "e5e60326f303db0db971",
+    "url": "/weather_app_1/static/css/main.8cdd2a9b.chunk.css"
+  },
+  {
+    "revision": "be92462b6a17c4d323fe",
+    "url": "/weather_app_1/static/js/2.01086832.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather_app_1/static/js/2.01086832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e60326f303db0db971",
+    "url": "/weather_app_1/static/js/main.1b6c6a17.chunk.js"
+  },
+  {
+    "revision": "fd64f61c4e691c3c70eb",
+    "url": "/weather_app_1/static/js/runtime-main.e8f27e32.js"
+  }
+]);
